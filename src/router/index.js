f885af3dee8a0aae0cbeebb74270d88d48d243f3
Divya Tagtachian <hello@shortdiv.com>
2020-04-14 15:39:12 -0500

    add custom route

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/success',
+    path: '/custom',
     name: 'Success',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
